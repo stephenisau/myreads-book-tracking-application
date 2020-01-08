31bebc1dc5bbbff377c956f1122e5e03c32ddeae
stephenau93 <stephenau93@gmail.com>
2020-01-07 21:07:08 -0800

    deploy to gh pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import * as BooksAPI from './BooksAPI'
+import './App.css'
+import { Route, Link } from 'react-router-dom'
+
+import Search from './Search';
+import Book from './Book';
+
+/*
+ Main application
+*/
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  async function fetchBooks() {
+    const res = await BooksAPI.getAll();
+    setBooks([...books, ...res])
+  }
+
+  const moveShelf = (book, shelf) => {
+    BooksAPI.update(book, shelf).then(resp => {
+      book.shelf = shelf;
+      setBooks(prevState => ([...prevState.map(b => b.id === book.id ? book : b)]));
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Route exact path='/' render={() => (
+        <MainPage books={books} moveShelf={moveShelf} />)} />
+      <Route exact path="/search" render={() => (
+        <Search books={books} moveShelf={moveShelf} />
+      )} />
     </div>
-  );
+  )
+}
+
+
+function MainPage({ books, moveShelf }) {
+
+  if (!books) return <React.Fragment>Loading...</React.Fragment>
+
+  const currentReads = books.filter(book => book.shelf === 'currentlyReading')
+  const wantToRead = books.filter(book => book.shelf === 'wantToRead')
+  const finishedReading = books.filter(book => book.shelf === 'read')
+
+
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+      <div className="list-books-content">
+        <div>
+          <Shelf moveShelf={moveShelf} name="Currently Reading" books={currentReads} />
+          <Shelf moveShelf={moveShelf} name="Want to Read" books={wantToRead} />
+          <Shelf moveShelf={moveShelf} name="Finished Reading" books={finishedReading} />
+        </div>
+      </div>
+      <div className="open-search">
+        <Link to="/search"><button>Add a book</button></Link>
+      </div>
+    </div>
+  )
+}
+
+function Shelf({ name, books, moveShelf }) {
+
+  const currBooks = books.length > 0 ? books.map((book, id) => <Book book={book} key={id} id={id} moveShelf={moveShelf} />) : <React.Fragment>No books...</React.Fragment>
+
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{name}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {currBooks}
+        </ol>
+      </div>
+    </div>
+  )
 }
 
 export default App;
